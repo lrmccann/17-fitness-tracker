600130233f8d5900ff29021d2ca3e6fff4c258e6
Logan McCAnn <loganrmccann@gmail.com>
2020-09-07 21:52:03 -0500

    first commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,56 @@
+const workout = require("../models/workout.js");
+
+
+exports.route = (app) => {
+app.put('/api/workouts/:id', (req, res) => {
+    let idParam = req.params;
+    let data = req.body;
+        workout.updateOne( {_id: idParam.id }, {$push: {exercises:  [
+            {
+                "type" : data.type,
+                "name" : data.name,
+                "duration" : data.duration,
+                "distance" : data.distance,
+                "weight" : data.weight,
+                "reps" : data.reps,
+                "sets" : data.sets
+            }
+        ] 
+    }}).then(dbUpdate => {
+        res.json(dbUpdate);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+  });
+
+  app.post('/api/workouts', (req, res) => {
+    workout.create({
+        day: new Date().setDate(new Date().getDate())
+    }).then(dbUpdate => {
+        res.json(dbUpdate)
+    }).catch(err => {
+        res.json(err)
+    });
+  });
+  
+
+app.get('/api/workouts/range', (req, res) => {
+        workout.find()
+        .then(workouts => {
+            res.json(workouts)
+        }).catch(err => {
+            res.json(err)
+        });
+  });
+  
+  app.get('/api/workouts', (req, res) => {
+    workout.find({})
+    .then(workouts => {
+        res.json(workouts)
+    }).catch(err => {
+        res.json(err)
+    });
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlroutes.js')
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+
+apiRoutes.route(app);
+htmlRoutes.route(app);
+
+app.listen(PORT, () => console.log(`API is listening on port ${PORT}`));
